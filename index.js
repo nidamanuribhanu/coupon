b3ee7224c68b1c7c9479cc8716f54b2db7c123f9
Nidamanuri Bhanu Prakash <95289280+nidamanuribhanu@users.noreply.github.com>
2022-06-24 17:30:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+function opencoupon(){
+    document.getElementById("coupon").style.visibility="visible";
+}
+function closecoupon(){
+    document.getElementById("coupon").style.visibility="hidden";
+}
